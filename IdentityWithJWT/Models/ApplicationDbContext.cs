8b75c3e921727a287149c5bae24c616c4c9a8c90
Tin Nguyen <tin.nguyen@mevry.com>
2020-03-04 09:08:23 +0700

    Add project files.

---
+++ IdentityWithJWT/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityWithJWT.Api.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
+
+/* 
+ * 
+ * IdentityDbContext khong thay xai` trong pcc?
+  
+     
+     
+     */
